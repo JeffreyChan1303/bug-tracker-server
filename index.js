40f691f1752fb6f771a10fffea006d40cbb33a3c
Jeffrey Chan <96323265+jfc2009@users.noreply.github.com>
2022-05-01 17:48:17 -0400

    initialize mongodb database

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 const app = express();
+
+// setting up bodyParser so it can properly send out requests
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// connect application with mongodb atlas
+
+// this should be stored in .env
+const CONNECTION_URL = 'mongodb+srv://jeffreychan:jeffreychan123@cluster0.wcviz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 3001;
+
+mongoose.connect(CONNECTION_URL, {})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch((error) => console.log(error.message));
