04c2b2b4ed63c45bb1c7b2908dcf7a4cdfa99b29
Jeffrey Chan <96323265+jfc2009@users.noreply.github.com>
2022-05-19 02:49:05 -0400

    added updateTicket route and env variables

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import dotenv from 'dotenv';
 import ticketRoutes from './routes/tickets.js';
 import projectRoutes from './routes/projects.js';
 
+
 const app = express();
+dotenv.config();
 
 // setting up bodyParser so it can properly send out requests
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -20,9 +23,9 @@ app.use('/projects', projectRoutes);
 // connect application with mongodb atlas
 
 // this should be stored in .env
-const CONNECTION_URL = 'mongodb+srv://jeffreychan:jeffreychan123@cluster0.wcviz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// const CONNECTION_URL = 'mongodb+srv://jeffreychan:jeffreychan123@cluster0.wcviz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 9000;
 
-mongoose.connect(CONNECTION_URL, {})
+mongoose.connect(process.env.CONNECTION_URL, {})
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
     .catch((error) => console.log(error.message));
--- routes/tickets.js
+++ routes/tickets.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get('/allTickets', getAllTickets);
 router.post('/createTicket', createTicket);
-router.put('/updateTicket/:id', updateTicket);
+router.patch('/updateTicket/:id', updateTicket);
 router.get('/ticketDetails/:id', getTicketDetails);
 router.delete('/deleteTicket/:id', deleteTicket);
 
